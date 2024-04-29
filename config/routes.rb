34c0e2fee53aaab3079ef53bd25a4300371bc41e
joemiddie13 <joemiddie13@gmail.com>
2024-04-29 11:55:39 -0700

    created new and edit actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root "movies#index"
-  get "up" => "rails/health#show", as: :rails_health_check
-  get "movies" => "movies#index"
-  get "movies/:id" => "movies#show", as: "movie"
+  
+  resources :movies
+  
+  # get "up" => "rails/health#show", as: :rails_health_check
+  # get "movies" => "movies#index"
+  # get "movies/:id" => "movies#show", as: "movie"
+  # get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+
+  # patch "movies/:id" => "movies#update"
 end
