8e10e0c7acd21f30a53eb03ec66dcb85317d723b
joemiddie13 <joemiddie13@gmail.com>
2024-05-01 10:33:23 -0700

    started to implement review system

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
   root "movies#index"
   
-  resources :movies
+  resources :movies do
+    resources :reviews
+  end
   
   # get "up" => "rails/health#show", as: :rails_health_check
   # get "movies" => "movies#index"
